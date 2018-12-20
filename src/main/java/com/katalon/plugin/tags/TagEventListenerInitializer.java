6d4b008a999c7ffabea2f7eb92a61969dcd938a1
Duy Luong <duyluong@kms-technology.com>
2018-12-20 17:31:14 +0700

    Update to 1.0.1

--- src/main/java/com/katalon/plugin/tags/TagEventListenerInitializer.java
+++ src/main/java/com/katalon/plugin/tags/TagEventListenerInitializer.java
@@ -1,7 +1,14 @@
 package com.katalon.plugin.tags;
 
-import com.katalon.platform.api.extension.event.EventListener;
-import com.katalon.platform.api.extension.event.EventListenerInitializer;
+import org.osgi.service.event.Event;
+
+import com.katalon.platform.api.event.EventListener;
+import com.katalon.platform.api.exception.ResourceException;
+import com.katalon.platform.api.extension.EventListenerInitializer;
+import com.katalon.platform.api.preference.PluginPreference;
+import com.katalon.platform.api.service.ApplicationManager;
+import com.katalon.platform.api.service.PreferenceManager;
+import com.katalon.platform.api.service.ProjectManager;
 
 /**
  * This plugin only activates filtering feature in KS.
@@ -10,6 +17,21 @@ public class TagEventListenerInitializer implements EventListenerInitializer {
 
     @Override
     public void registerListener(EventListener eventListener) {
+
+        eventListener.on(Event.class, e -> {
+            try {
+                PreferenceManager preferenceManager = ApplicationManager.getInstance().getPreferenceManager();
+                ProjectManager projectManager = ApplicationManager.getInstance().getProjectManager();
+
+                PluginPreference pluginPreference = preferenceManager.getPluginPreference(
+                        projectManager.getCurrentProject().getId(), "com.katalon.katalon-studio-tags-plugin");
+                pluginPreference.getBoolean("haha", true);
+            } catch (ResourceException e1) {
+                // TODO Auto-generated catch block
+                e1.printStackTrace();
+            }
+
+        });
     }
 
 }
